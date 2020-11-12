4e9fb7a6a101e2397496192ae446f35f607b7de9
Sébastien Pécoul <specoul@steamulo.com>
2020-11-12 18:09:10 +0100

    [ADD] Async implem

--- SPSample/scenes/MultiWSProcess/MultiWSProcessViewController.swift
+++ SPSample/scenes/MultiWSProcess/MultiWSProcessViewController.swift
@@ -21,18 +21,28 @@ class MultiWSProcessViewController: UIViewController {
     test = Warehouse(processes: [FirstProcessX(), SecondProcess(), ThirdProcess()])
  
     
-    
-    // Promise
-    PromiseLiteConfiguration.debugger = DefaultPromiseLiteDebugger { print($0) }
-    promiseFirstTask()
-      .flatMap { self.promiseSecondTask(model:$0) }
-      .flatMap { self.promiseThirdTask(model:$0) }
-      .map { debugPrint($0) }
+//    // Promise
+//    PromiseLiteConfiguration.debugger = DefaultPromiseLiteDebugger { print($0) }
+//    promiseFirstTask()
+//      .flatMap { self.promiseSecondTask(model:$0) }
+//      .flatMap { self.promiseThirdTask(model:$0) }
+//      .map { debugPrint($0) }
   }
   
   
   @IBAction func launchProcess() {
-    test.launch() {
+//    test.launch() {
+//      switch $0 {
+//      case .success(let model):
+//        debugPrint("☺️")
+//        debugPrint(model)
+//      case .failure:
+//        debugPrint("🤬")
+//        debugPrint("Error")
+//      }
+//    }
+    
+    operationManager.load(operations: [.mostViewedProgram]) {
       switch $0 {
       case .success(let model):
         debugPrint("☺️")
@@ -43,24 +53,44 @@ class MultiWSProcessViewController: UIViewController {
       }
     }
     
-    operationManager.launch {
-      switch $0 {
-      case .success(let model):
-        debugPrint("☺️")
-        debugPrint(model)
-      case .failure:
-        debugPrint("🤬")
-        debugPrint("Error")
+    DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
+      
+      self.operationManager.update(operations: [.favorite, .lastPlayedEpisodes]) {
+        switch $0 {
+        case .success(let model):
+          debugPrint("☺️")
+          debugPrint(model)
+        case .failure:
+          debugPrint("🤬")
+          debugPrint("Error")
+        }
       }
     }
   }
 }
 
-struct Model {
+class Model {
   let id: Int
   let name: String
   let count: Int
   let rating: Int
+  
+  init(id: Int, name: String, count: Int, rating: Int) {
+    self.id = id
+    self.name = name
+    self.count = count
+    self.rating = rating
+  }
+  
+  func merge(with model: Model) -> Model {
+    return Model(
+      id: id,
+      name: name + model.name,
+      count: count + model.count,
+      rating: rating + model.rating
+    )
+  }
+  
 }
 
 extension Model: CustomDebugStringConvertible {
@@ -228,12 +258,138 @@ extension MultiWSProcessViewController {
 // MARK: Opération implem
 
 extension MultiWSProcessViewController {
-
+  
+  class Storage {
+    var model: Model?
+    
+    init() {
+      self.model = nil
+    }
+  }
   
   class OperationManager {
     
-    var model: Model?
     
+    enum Operation {
+      case homeFetching
+      case favorite
+      case mostViewedProgram
+      case lastPlayedEpisodes
+    }
+    
+    
+    let storage = Storage()
+    
+    
+    
+    func load(operations: [Operation], completion: @escaping (Result<Model, SampleError>) -> Void) {
+      
+      DispatchQueue.global(qos: .userInteractive).async {
+        
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 3
+    
+        let homeOperation = self.operation(.homeFetching)
+        
+        let otherOperations = operations.map { self.operation($0) }
+        otherOperations.forEach { $0.addDependency(homeOperation) }
+        queue.addOperations([homeOperation] + otherOperations, waitUntilFinished: true)
+
+        DispatchQueue.main.async { [self] in
+          if let model = storage.model {
+            completion(.success(model))
+          } else {
+            completion(.failure(SampleError(code: 0)))
+          }
+        }
+      }
+    }
+    
+    func update(operations: [Operation], completion: @escaping (Result<Model, SampleError>) -> Void) {
+      DispatchQueue.global(qos: .userInteractive).async {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 3
+        let otherOperations = operations.map { self.operation($0) }
+        queue.addOperations(otherOperations, waitUntilFinished: true)
+        DispatchQueue.main.async { [self] in
+          if let model = storage.model {
+            completion(.success(model))
+          } else {
+            completion(.failure(SampleError(code: 0)))
+          }
+        }
+      }
+    }
+    
+    // MARK:
+    
+    func operation(_ operation: Operation) -> AsyncOperation {
+      switch operation {
+      case .favorite:
+        return favoriteOperation()
+      case .homeFetching:
+        return FirstOperation() { operation, result in
+          switch result {
+          case .failure:
+            operation?.cancel()
+            operation?.finish()
+          case .success(let model):
+            self.storage.model = model
+          }
+          debugPrint("End 1")
+          operation?.finish()
+        }
+      case .lastPlayedEpisodes:
+        return resumeOperation()
+      case .mostViewedProgram:
+        return topOperation()
+      }
+    }
+    
+    func favoriteOperation() -> AsyncOperation {
+      return SecondOperation(storage: storage) { operation, result in
+        
+        switch result {
+        case .failure:
+          break
+        case .success(let model):
+          self.storage.model = self.storage.model!.merge(with: model)
+        }
+        
+        operation?.finish()
+      }
+    }
+    
+    func topOperation() -> AsyncOperation {
+      return FourthOperation(storage: storage) { operation, result in
+        
+        switch result {
+        case .failure:
+          break
+        case .success(let model):
+          self.storage.model = self.storage.model!.merge(with: model)
+        }
+        
+        operation?.finish()
+      }
+    }
+    
+    func resumeOperation() -> AsyncOperation {
+      return ThirdOperation(storage: storage) { operation, result in
+       
+        
+        switch result {
+        case .failure:
+          break
+        case .success(let model):
+          self.storage.model = self.storage.model!.merge(with: model)
+        }
+        
+        operation?.finish()
+      }
+    }
+    
+    /*
     func launch(completion: @escaping (Result<Model, SampleError>) -> Void) {
       
       DispatchQueue.global(qos: .userInteractive).async {
@@ -241,13 +397,29 @@ extension MultiWSProcessViewController {
         let queue = OperationQueue()
         queue.maxConcurrentOperationCount = 3
         
-        let operation2 = SecondOperation() { operation, result in
+        let operation2 = SecondOperation(input: self.model) { operation, result in
           debugPrint("End 2")
+          
+          switch result {
+          case .failure:
+            break
+          case .success(let model):
+            self.model = self.model!.merge(with: model)
+          }
+          
           operation?.finish()
         }
         
-        let operation3 = ThirdOperation() { operation, result in
+        let operation3 = ThirdOperation(input: ) { operation, result in
           debugPrint("End 3")
+          
+          switch result {
+          case .failure:
+            break
+          case .success(let model):
+            self.model = self.model!.merge(with: model)
+          }
+          
           operation?.finish()
         }
         
@@ -257,9 +429,9 @@ extension MultiWSProcessViewController {
             operation?.cancel()
             operation?.finish()
           case .success(let model):
-            operation2.input = model
-            operation3.input = model
+            self.model = model
           }
+          
           debugPrint("End 1")
           operation?.finish()
         }
@@ -268,11 +440,18 @@ extension MultiWSProcessViewController {
         operation3.addDependency(operation1)
         queue.addOperations([operation1, operation2, operation3], waitUntilFinished: true)
 
-        DispatchQueue.main.async {
-          completion(.failure(SampleError(code: 0)))
+        DispatchQueue.main.async { [self] in
+          
+          if let model = storage.model {
+            completion(.success(model))
+          } else {
+            
+            completion(.failure(SampleError(code: 0)))
+         
+          }
         }
       }
-    }
+    }*/
   }
   
   class FirstOperation: AsyncOperation {
@@ -301,9 +480,10 @@ extension MultiWSProcessViewController {
   class SecondOperation: AsyncOperation {
   
     let completion: (AsyncOperation?, Result<Model, SampleError>) -> Void
-    var input: Model?
+    let storage: Storage
     
-    init(completion: @escaping (AsyncOperation?, Result<Model, SampleError>) -> Void) {
+    init(storage: Storage, completion: @escaping (AsyncOperation?, Result<Model, SampleError>) -> Void) {
+      self.storage = storage
       self.completion = completion
     }
     
@@ -315,7 +495,7 @@ extension MultiWSProcessViewController {
           return
       }
       
-      secondTask(model: input!) { result in
+      secondTask(model: storage.model!) { result in
         self.completion(self, result)
       }
       
@@ -325,9 +505,10 @@ extension MultiWSProcessViewController {
   class ThirdOperation: AsyncOperation {
   
     let completion: (AsyncOperation?, Result<Model, SampleError>) -> Void
-    var input: Model?
+    let storage: Storage
     
-    init(completion: @escaping (AsyncOperation?, Result<Model, SampleError>) -> Void) {
+    init(storage: Storage, completion: @escaping (AsyncOperation?, Result<Model, SampleError>) -> Void) {
+      self.storage = storage
       self.completion = completion
     }
     
@@ -339,12 +520,38 @@ extension MultiWSProcessViewController {
           return
       }
       
-      thirdTask(model: input!) { result in
+      thirdTask(model: storage.model!) { result in
         self.completion(self, result)
       }
       
     }
   }
+  
+  class FourthOperation: AsyncOperation {
+  
+    let completion: (AsyncOperation?, Result<Model, SampleError>) -> Void
+    let storage: Storage
+    
+    init(storage: Storage, completion: @escaping (AsyncOperation?, Result<Model, SampleError>) -> Void) {
+      self.storage = storage
+      self.completion = completion
+    }
+    
+    override func main() {
+      guard !isCancelled,
+        dependencies.filter({ $0.isCancelled }).isEmpty else {
+          cancel()
+          finish()
+          return
+      }
+      
+      fourthTask(model: storage.model!) { result in
+        self.completion(self, result)
+      }
+      
+    }
+  }
+  
 }
 
 // MARK:
@@ -352,22 +559,32 @@ extension MultiWSProcessViewController {
 
 func firstTask(completion: @escaping ((Result<Model, SampleError>) -> Void)) {
   DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-    //completion(.success(Model(id: 0, name: "A name", count: 1, rating: 0)))
-    completion(.failure(SampleError(code: 0)))
+    completion(.success(Model(id: 0, name: "A name", count: 1, rating: 0)))
+    //completion(.failure(SampleError(code: 0)))
     
   }
 }
 
 func secondTask(model: Model, completion: @escaping ((Result<Model, SampleError>) -> Void)) {
   DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-    completion(.failure(SampleError(code: 0)))
-    //completion(.success(Model(id: 0, name: "A name", count: model.count * 3, rating: 0)))
+    //completion(.failure(SampleError(code: 0)))
+    debugPrint("Favorite finished")
+    completion(.success(Model(id: 0, name: "Simon", count: 100, rating: 100)))
   }
 }
 
 func thirdTask(model: Model, completion: @escaping ((Result<Model, SampleError>) -> Void)) {
-  DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-    completion(.failure(SampleError(code: 0)))
-    //completion(.success(Model(id: 0, name: "A name", count: model.count * 5, rating: 0)))
+  DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+    //completion(.failure(SampleError(code: 0)))
+    debugPrint("Resume finished")
+    completion(.success(Model(id: 0, name: "Peppa", count: 4, rating: -2)))
+  }
+}
+
+func fourthTask(model: Model, completion: @escaping ((Result<Model, SampleError>) -> Void)) {
+  DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+    debugPrint("Top media finished")
+    //completion(.failure(SampleError(code: 0)))
+    completion(.success(Model(id: 0, name: "Peppa", count: 1000, rating: -2)))
   }
 }
